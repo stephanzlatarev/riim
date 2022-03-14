36c5971772fc4682192caa4ebd2d5c17ad20d083
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-14 18:37:15 +0200

    Add main menu

--- player.js
+++ player.js
@@ -1,8 +1,31 @@
+let scene = 0;
+
 const SCENES = [
+  "main-menu",
   "intro-a",
 ];
+const MENU = {
+  "start-new-game": 1, // intro-a
+};
 
 const ACTIONS = {
+  "menu": function(action) {
+    const button = $("<div>")
+    .css("position", "absolute").css("top", "50%").css("left", "30%").css("width", "40%")
+    .css("color", "white").css("font-size", "300%")
+    .css("cursor", "pointer")
+    .appendTo($("body"));
+    button.text(action.items[0].label);
+    return new Promise(function(resolve) {
+      button.click(function() {
+        console.log("Process action", action.items[0].action);
+        scene = MENU[action.items[0].action];
+        button.remove();
+        resolve();
+      });
+    });
+  },
+
   "speech": async function(action) {
     const bubble = $("<div>")
       .css("position", "absolute").css("top", "50%").css("left", "30%").css("width", "60%")
@@ -11,8 +34,16 @@ const ACTIONS = {
 
     for (const line of action.text) {
       console.log(action.actor, ":", line);
-      bubble.append($("<p>").text(line));
+
+      const utterance = new SpeechSynthesisUtterance(line);
+      speechSynthesis.speak(utterance);
+
       await wait(1000);
+      bubble.append($("<p>").text(line));
+
+      await new Promise(function(resolve) {
+        utterance.addEventListener("end", resolve);
+      });
     }
   }
 };
@@ -39,10 +70,10 @@ async function play(scene) {
   }
 }
 
-async function go() {
-  for (const scene of SCENES) {
-    await play(await load(scene));
+async function loop() {
+  while (scene < SCENES.length) {
+    await play(await load(SCENES[scene++]));
   }
 }
 
-$(document).ready(go);
+$(document).ready(loop);
