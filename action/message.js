0fe5d137bcc811c949bb21cacc8d08f567333a2b
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-17 23:09:46 +0200

    Chat messages bubble up

--- action/message.js
+++ action/message.js
@@ -11,18 +11,13 @@ export function cancel() {
 }
 
 export default async function(message) {
-  const bubble = $("<div>")
-    .css("position", "absolute").css("top", "50%").css("left", "30%").css("width", "60%")
-    .css("color", "white").css("font-size", "150%")
-    .appendTo(chat);
-
   console.log(message.actor, ":", message.text);
 
   const utterance = new SpeechSynthesisUtterance(message.text);
   speechSynthesis.speak(utterance);
 
   await wait(1000);
-  bubble.append($("<p>").text(message.text));
+  $("<div>").addClass("message").appendTo(chat).text(message.text);
 
   await new Promise(function(resolve) {
     utterance.addEventListener("end", resolve);
--- layout.js
+++ layout.js
@@ -5,10 +5,13 @@ $(document).ready(function() {
 
   $("#stats").css("position", "absolute").css("z-index", "2")
     .css("width", "100%").css("height", "3rem");
-  
+
   $("#buttons").css("position", "absolute").css("z-index", "3")
     .css("top", "3rem").css("left", "90%").css("width", "10%").css("height", "3rem");
-  
+
+  $("<style type='text/css'>.message { margin: 0.5rem; padding: 0.5rem; background: rgba(0, 0, 0, 0.5); border-radius: 0.5rem; }</style>").appendTo("head");
   $("#chat").css("position", "absolute").css("z-index", "4")
-    .css("width", "50%").css("height", "100%");
+    .css("top", "3rem").css("left", "30%").css("width", "50%").css("height", "80%")
+    .css("display", "flex").css("flex-direction", "column").css("justify-content", "flex-end")
+    .css("color", "white").css("font-size", "150%");
 });
