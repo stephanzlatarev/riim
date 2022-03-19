166dd15c59a83c6723a9a94ef36d6041c8cfff4b
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-20 00:13:49 +0200

    User cannot select text of button labels

--- layout.js
+++ layout.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   $("#buttons").css("position", "absolute").css("z-index", "4")
     .css("top", "3rem").css("left", "90%").css("width", "10%").css("height", "90%")
     .css("display", "flex").css("flex-direction", "column").css("justify-content", "flex-end")
-    .css("color", "white").css("font-size", "200%");
+    .css("color", "white").css("font-size", "200%").css("user-select", "none");
 
   $("<style type='text/css'>.message { margin: 0.5rem; padding: 0.5rem; background: rgba(0, 0, 0, 0.5); border-radius: 0.5rem; }</style>").appendTo("head");
   $("#chat").css("position", "absolute").css("z-index", "5")
