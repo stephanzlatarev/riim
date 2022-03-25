e7c542ec7157fa8b2f9f48ea2faadbd438eea81d
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-25 23:52:57 +0200

    Add menu button icons

--- action/menu-button.js
+++ action/menu-button.js
@@ -8,7 +8,13 @@ export function cancel() {
 export default function(action, start, perform) {
   const button = $("<div>").addClass("menu-button").appendTo(menu.show());
 
-  button.text(action.label);
+  if (action.icon) {
+    $("<img>").attr("src", "./image/" + action.icon)
+      .css("width", "4vh").css("margin", "-1vh 0.3em -1vh -0.1em")
+      .appendTo(button);
+  }
+
+  $("<span>").text(action.label).appendTo(button);
 
   button.click(function() {
     if (action.scene) {
