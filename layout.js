6ad647f1cbb84caf63d9c74471b039711d688dbd
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-25 13:33:33 +0200

    Change font of menu to Black Ops One

--- layout.js
+++ layout.js
@@ -15,12 +15,12 @@ $(document).ready(function() {
     .css("display", "flex").css("flex-direction", "column").css("justify-content", "flex-end")
     .css("color", "white").css("font-size", "200%").css("user-select", "none");
 
-  $("<style type='text/css'>.menu-title { margin: 0.5rem; padding: 0.5rem; font-family: Papyrus; font-size: 120%; font-weight: bold; }</style>").appendTo("head");
-  $("<style type='text/css'>.menu-button { margin: 0.5rem; padding: 0.5rem; background-image: linear-gradient(173deg, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.5)); cursor: pointer; }</style>").appendTo("head");
+  $("<style type='text/css'>.menu-title { margin: 2vh 0px; padding: 0px 1em; font-size: 120%; font-weight: bold; }</style>").appendTo("head");
+  $("<style type='text/css'>.menu-button { margin: 2vh 0px 0px 0px; padding: 1vh 0.5em; background-image: linear-gradient(173deg, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.5)); cursor: pointer; }</style>").appendTo("head");
   $("#menu").css("position", "absolute").css("z-index", "5")
-    .css("top", "20%").css("left", "30%").css("width", "30%").css("height", "60%")
+    .css("top", "20vh").css("left", "30vw").css("width", "20em").css("height", "60vh")
     .css("display", "flex").css("flex-direction", "column")
-    .css("color", "white").css("font-size", "150%").css("user-select", "none")
+    .css("color", "white").css("font-family", "Black Ops One").css("font-size", "3vh").css("user-select", "none")
     .hide();
 
   $("<style type='text/css'>.message { margin: 0.5rem; padding: 0.5rem; background: rgba(0, 0, 0, 0.5); border-radius: 0.5rem; }</style>").appendTo("head");
