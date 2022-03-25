70847476b9e5e721ce27948bb127b5b476195d6e
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-25 22:49:08 +0200

    Imroved text sizes

--- layout.js
+++ layout.js
@@ -1,4 +1,9 @@
 
+const ROW_SIZE = "7vh";
+const LABEL_FONT_SIZE = "3vh";
+const TEXT_FONT_SIZE = "2.75vh";
+const BLOCK_STYLE = "width: 10em; margin: 1vh 2vh 0 0; padding: 0.25em 0px; background: rgba(0, 0, 0, 0.75); text-align: center;";
+
 $(document).ready(function() {
   $("#background").css("position", "absolute").css("z-index", "1")
     .css("width", "100%").css("height", "100%");
@@ -6,30 +11,30 @@ $(document).ready(function() {
   $("#foreground").css("position", "absolute").css("z-index", "2")
     .css("width", "100%").css("height", "100%");
 
-  $("<style type='text/css'>.stat { width: 10em; margin: 3px; padding: auto; background: rgba(0, 0, 0, 0.75); border: white 1px solid; border-radius: 4px; text-align: center; }</style>").appendTo("head");
+  $("<style type='text/css'>.stat { " + BLOCK_STYLE + " border: white 0.25vh dotted; border-radius: 0.2em; }</style>").appendTo("head");
   $("#stats").css("position", "absolute").css("z-index", "3")
-    .css("width", "100%").css("height", "4vh")
+    .css("width", "100%").css("height", ROW_SIZE)
     .css("display", "flex").css("flex-direction", "row").css("justify-content", "flex-end")
-    .css("color", "white").css("font-family", "Tahoma").css("font-size", "2vh").css("user-select", "none");
+    .css("color", "white").css("font-family", "Tahoma").css("font-size", LABEL_FONT_SIZE).css("user-select", "none");
 
   $("<style type='text/css'>.menu-title { margin: 2vh 0px; padding: 0px 1em; font-size: 120%; font-weight: bold; }</style>").appendTo("head");
   $("<style type='text/css'>.menu-button { margin: 2vh 0px 0px 0px; padding: 1vh 0.5em; background-image: linear-gradient(173deg, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.5)); cursor: pointer; }</style>").appendTo("head");
   $("#menu").css("position", "absolute").css("z-index", "5")
     .css("top", "20vh").css("left", "30vw").css("width", "20em").css("height", "60vh")
     .css("display", "flex").css("flex-direction", "column")
-    .css("color", "white").css("font-family", "Black Ops One").css("font-size", "3vh").css("user-select", "none")
+    .css("color", "white").css("font-family", "Black Ops One").css("font-size", LABEL_FONT_SIZE).css("user-select", "none")
     .hide();
 
-  $("<style type='text/css'>.message { margin: 0.5rem; padding: 0.5rem; background: rgba(0, 0, 0, 0.5); border-radius: 0.5rem; }</style>").appendTo("head");
+  $("<style type='text/css'>.message { margin: 0.5em 0.5em 0 0; padding: 0.5em; background: rgba(0, 0, 0, 0.5); border-radius: 0.5em; }</style>").appendTo("head");
   $("#chat").css("position", "absolute").css("z-index", "5")
-    .css("top", "3rem").css("left", "30%").css("width", "50%").css("height", "80%")
+    .css("top", ROW_SIZE).css("left", "30%").css("width", "50%").css("height", "80%")
     .css("display", "flex").css("flex-direction", "column").css("justify-content", "flex-end")
-    .css("color", "white").css("font-size", "150%")
+    .css("color", "white").css("font-size", TEXT_FONT_SIZE)
     .hide();
 
-  $("<style type='text/css'>.button { margin: 4vh 5px; padding: 0.25em 0px; background: rgba(0, 0, 0, 0.75); border: white 3px solid; border-radius: 1rem; cursor: pointer; text-align: center; }</style>").appendTo("head");
+  $("<style type='text/css'>.button { " + BLOCK_STYLE + " border: white 0.25vh solid; border-radius: 1em; cursor: pointer; }</style>").appendTo("head");
   $("#buttons").css("position", "absolute").css("z-index", "10")
-    .css("top", "5vh").css("left", "calc(98vw - 10em)").css("width", "10em").css("height", "90vh")
+    .css("top", ROW_SIZE).css("left", "calc(98vw - 10em)").css("width", "10em").css("height", "calc(100vh - " + ROW_SIZE + " - " + ROW_SIZE + ")")
     .css("display", "flex").css("flex-direction", "column").css("justify-content", "flex-end")
-    .css("color", "white").css("font-family", "Tahoma").css("font-size", "3vh").css("user-select", "none");
+    .css("color", "white").css("font-family", "Tahoma").css("font-size", LABEL_FONT_SIZE).css("user-select", "none");
 });
