8576e1dbf22e505b7351b6b2c141babf0ae4a0cd
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-27 18:01:28 +0300

    Remove warning.js

--- action/background.js
+++ action/background.js
@@ -7,7 +7,7 @@ export default function(background) {
   if (background.front) {
     const image = $("<img>").attr("src", "./background/" + background.image).css("width", "100%").css("height", "100%");
 
-    $("#foreground").css("background", "black").empty().append(image).show();
+    $("#foreground").css("background", "#666666").empty().append(image).show();
   } else {
     $("#background").css("background-image", "url('./background/" + background.image + "')");
   }
