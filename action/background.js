096a7a6b962e35b1eeb19f8a705f022f6b74f797
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-03-27 17:55:41 +0300

    Add world map as default

--- action/background.js
+++ action/background.js
@@ -1,4 +1,14 @@
 
+export function cancel() {
+  $("#foreground").hide();
+}
+
 export default function(background) {
-  $("#background").css("background-image", "url('./background/" + background.image + "')");
+  if (background.front) {
+    const image = $("<img>").attr("src", "./background/" + background.image).css("width", "100%").css("height", "100%");
+
+    $("#foreground").css("background", "black").empty().append(image).show();
+  } else {
+    $("#background").css("background-image", "url('./background/" + background.image + "')");
+  }
 }
